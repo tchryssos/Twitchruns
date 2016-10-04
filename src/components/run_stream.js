74c33b872b2eaa2c587bf5ffbe494287d6ae9370
github username <github email address>
2016-10-04 10:51:26 -0400

    Created empty Stream Components

---
+++ src/components/run_stream.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import RunNail from './run_nail'
+import SideBar from './sibe_bar'
+import SelectedStream from './selected_stream'
+
+export default class RunStream extends React.Component{
+
+  render(){
+
+  }
+}
