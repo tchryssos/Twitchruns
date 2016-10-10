3c7dd58e8328c50f7099eafa5c68fe0885bc8379
Ece Ozalp <github email address>
2016-10-09 23:07:17 -0400

    new carousel almost implemented, will fix the position of buttons

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/nav_bar'
 import RunStream from './components/run_stream'
-import FeaturedBar from './components/featured_bar'
+import NewCarousel from './components/new_carousel_test'
+// import FeaturedBar from './components/featured_bar'
 
 class App extends Component {
+ 
 
   render() {
     return (
       <div className="App">
         < NavBar/>
-        <FeaturedBar />
-        <div>
-        {this.props.children}
-        </div>
-
+        < NewCarousel />
       </div>
     );
   }
