2ca3146b50aa1a223f8c99ef58648dd73ab3c276
github username <github email address>
2016-10-10 19:50:58 -0400

    add search reduction logic

--- src/reducers/runs_reducer.js
+++ src/reducers/runs_reducer.js
@@ -3,8 +3,14 @@ export default function runsReducer(state={}, action) {
     case 'FETCH_RUNS':
       return  {original: action.payload, filtered: action.payload}
     case 'FETCH_SEARCH':
-      debugger
-      return {original: state.original, filtered: state.original.filter((run)=>{return run.game.name.toLowerCase().includes(action.payload.toLowerCase())})}
+      let term = action.payload.toLowerCase()
+      return {original: state.original,
+              filtered: state.original.filter((run)=>{
+                debugger
+                      return run.game.name.toLowerCase().includes(term) ||
+                      run.run_placement.category.toLowerCase().includes(term) ||
+                      run.runner.username.toLowerCase().includes(term)
+                    })}
     default:
       return state
   }
