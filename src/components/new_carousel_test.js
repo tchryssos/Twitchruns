59e8ab583a0328ecacbf944dd109aa48802fbd41
Troy Chryssos <github email address>
2016-10-13 15:43:59 -0400

    changed instructions

--- src/components/new_carousel_test.js
+++ src/components/new_carousel_test.js
@@ -50,7 +50,7 @@ function NewCarousel (props) {
               <Carousel slidesToShow={6} cellSpacing={20} easing="easeInQuad" slidesToScroll={5} dragging={true} cellAlign={'left'} >
                 {FeaturedVideoList}
                 </Carousel>
-                <div className='row'><h3>Select a speedrun from above or filter by game, category, or runner!</h3></div>
+                <div className='row'><h3>Select a speedrun from above or filter by game, category, or runner! <br/> Click the Twitchruns console to return to the browser at any point. </h3></div>
               </div>)
         }
         else {
