e9e66f4fd9d5f3724d7c7ceac5c6326868b77733
github username <github email address>
2016-10-13 15:03:14 -0400

    Search bar fixes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,15 +13,19 @@ class SearchBar extends React.Component{
     this.props.actions.filterRuns(this.refs.searchTerm.value)
   }
 
+  stopSearch(event){
+    event.preventDefault();
+  }
+
 
   render(){return (
     <div className="container">
         <div className="row">
             <div className="col-md-4 col-md-offset-3" style={{float: 'right'}}>
-                <form action="" className="search-form">
+                <form action="" className="search-form" onSubmit={(e)=> e.preventDefault()}>
                     <div className="form-group has-feedback">
                 		<label htmlFor="search" className="sr-only">Search</label>
-                		<input type="text" className="form-control" name="search" ref='searchTerm' onChange={()=>{this.searchRuns()}} id="search" placeholder="search"/>
+                		<input type="text" className="form-control" name="search" ref='searchTerm'  onChange={()=>{this.searchRuns()}} id="search" placeholder="search"/>
                   		<span className="glyphicon glyphicon-search form-control-feedback"></span>
                 	</div>
                 </form>
